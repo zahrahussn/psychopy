a05e92cfd1b38d322d35ed21c2a0425395fe715a
Emanuel El Hadi <emanuelelhadi@Emanuels-MacBook-Pro.local>
2022-03-10 11:47:48 +0200

    Added v2 of the task and added other stimuli. Added mask and RT on first version.

--- faceMatch/faceTaskv2.py
+++ faceMatch/faceTaskv2.py
@@ -11,7 +11,7 @@ variable=''
 params = {'ID number':'1', 'lang':'eng',
 	 'frameRate':60,'duration':0.3,'maskduration':0.1,'blankduration':0.2,'response': 1.0, 'fp': 1.0,'task':'FaceMatch'}
 
-dlg = gui.DlgFromDict(params, title='Face Match v1', fixed=['dateStr'])
+dlg = gui.DlgFromDict(params, title='Face Match v2', fixed=['dateStr'])
 if dlg.OK:
     1+1 #toFile('lastParams.pickle', params) #save params to file for next time
 else:
@@ -19,15 +19,7 @@ else:
 
 fileName = 'data/FaceMatch'+params['ID number']+'_FaceMatch'     #Accessing data file to record data
 dataFile = open(fileName+'.txt', 'a')
-dataFile.write('nTrials, imagefileName, trials.thisTrial.field, thisResponse, accuracy, RT')   #Data file listing #Is this enough to create a data file?
-#if os.path.exists(fileName+'.txt')
-#    ii=1
-#    while True:
-#    new_name = fileName + "_" + str(ii) + '.txt'
-#    if not os.path.exists(new_name):
-#       fileName = new_name
-#       break 
-#    ii += 1
+dataFile.write('nTrials, trials.thisTrial.image, trials.thisTrial.field, thisResponse, accuracy, RT')    #Data file listing #Is this enough to create a data file?
 
 win = visual.Window(fullscr=True, allowGUI= True, monitor = 'testMonitor', units = 'deg')
 #Graphic User Interface True
@@ -40,39 +32,46 @@ fixation = visual.PatchStim(win, color=-1, tex=None, mask='cross',size=1.0 ,unit
 corSnd = sound.Sound(1800, octave=14, secs=0.01) 
 incorSnd = sound.Sound(800, octave=7, secs=0.01)
 
-facePath='Stimuli/' #Retrieve faces stimuli from the Stimuli folder with their respective numbers, 5 male 5 female
+facePath='Stimuli/' #Retriever face stimuli from the Stimuli folder with their respective numbers, 5 male 5 female
 imageM=[os.path.join(facePath+'M1'),os.path.join(facePath+'M2'),os.path.join(facePath+'M3'),os.path.join(facePath+'M4'),os.path.join(facePath+'M5')]
 imageF=[os.path.join(facePath+'F1'),os.path.join(facePath+'F2'),os.path.join(facePath+'F3'),os.path.join(facePath+'F4'),os.path.join(facePath+'F5')]
+
+distPath='Distractor/' #Retrieving distractors from Distractor folder with their respective numbers
+imageDist=[os.path.join(distPath+'D1'),os.path.join(distPath+'D2'),os.path.join(distPath+'D3'),os.path.join(distPath+'D4'),os.path.join(distPath+'D5'),os.path.join(distPath+'D6'),os.path.join(distPath+'D7'),os.path.join(distPath+'D8'),os.path.join(distPath+'D9'),os.path.join(distPath+'D10')]
 
 faceWidth=6.5
 faceHeight=6.5
 faceSize=(faceWidth,faceHeight)
-posLVF=((-2.5-faceWidth/2),0)      
-posRVF=((2.5+faceWidth/2),0)
+posLVF=((-3.5-faceWidth/2),0)      
+posRVF=((3.5+faceWidth/2),0)
 
 #Gaussian noise mask
-
 noiseTexture = numpy.random.rand(128, 128) * 2.0 - 1
 maskRight = visual.GratingStim(win, tex=noiseTexture, size=(6.5,6.5),pos=posRVF, units='deg', interpolate=False, autoLog=False)
-maskLeft= visual.GratingStim(win, tex=noiseTexture,size=(6.5, 6.5), pos=posLVF, units='deg', interpolate=False, autoLog=False)
+maskLeft= visual.GratingStim(win, tex=noiseTexture,size=(6.5, 6.5), pos=posLVF, units='deg', interpolate=False, autoLog=False) #do we have 
 
+
+#Draws a rectangular cue for face presentation
+cueVertices = [[(-.09,-.09),(-.09,.09),(.09,.09),(.09,-.09)],[(-.091,-.091),(-.091,.091),(.091,.091),(.091,-.091)]]
+cueRight = ShapeStim(win, vertices=cueVertices, units='deg', fillColor='red', lineWidth=0.05, size=37, pos=posRVF)
+cueLeft = ShapeStim(win, vertices=cueVertices, units='deg', fillColor='red', lineWidth=0.05, size=37, pos=posLVF)
 
 #Trial Handler randomizes and puts things in sequence the way you want them to be
 stimList=[]
 for image in range (0,len(imageM)):    #len is length. images picked from 0 to len(imageM) so basically up to 5 random images.
-    for field in [0,1]: 
+    for field in [0,1]: # 2 conditions RVF and LVF (Should it be counterbalanced(M and F 50%?)) (Does gender matter, putting all in one list)
         for gender in [0,1]: 
             stimList.append({'gender':gender, 'field':field, 'image':image}) # setting trial sequence,gender for gender, field for visual field, images of course
-trials = data.TrialHandler(stimList, 1) #Changed because she needs 100 trials. 5x20 is 100
+trials = data.TrialHandler(stimList, 5) #Changed because she needs 100 trials. 5x20 is 100
 trials.data.addDataType('RT')
 trials.data.addDataType('response') #no need for repetition time if we already specified (ask)
-trials.data.addDataType('accuracy')    #Do we add this here?
-clockRT = core.Clock()  #Records reaction time
+trials.data.addDataType('accuracy')                 #Do we add this here?
+clockRT = core.Clock() #Records reaction time
 
-RTs=''
-accuracy=''
+
 response=''
-
+accuracy=''
+RTs=''
 nTrials=0
 
 myMouse = event.Mouse() #adding mouse. Mouse coordinates are doubled for reasons we do not know. 
@@ -80,11 +79,12 @@ myMouse.clickReset() #resets at left click
 
 for key in ['escape']: #press escape to quit the experiment whenever
     event.globalKeys.add(key, func=core.quit)
-
-nTrials = 0
+    
 for thisTrial in trials: #Records the trials that are happening with this particular experiment. Put it after trial handler
     nTrials=nTrials+1 #Count which trial we're on
+
     # prepare face screen
+#    faceName=os.path.split(imageM[trials.thisTrial.image])[1] #trials.thisTrial.image means any image inside, faceName for just getting (M1) itself without the full system path. [1] (get the file name)
     if trials.thisTrial.gender==1:  #face is male
         IM='M'
         imagefileName=imageM[trials.thisTrial.image]+'.jpg' #trials.thisTrial.image is 5 items spanning from 0 to 4 
@@ -96,12 +96,18 @@ for thisTrial in trials: #Records the trials that are happening with this partic
 
     if trials.thisTrial.field==1: #  LVF trial 
         position=posLVF 
+        distRight = visual.ImageStim(win, image=imageDist[trials.thisTrial.image]+'.jpg', pos=posRVF,size=faceSize) #defining and retrieving distractor for RVF
+        dist=distRight
+        cue=cueLeft
+        maskLeft
         IM_location='LVF' #IM is basically images all
-        mask=maskLeft
     else: # RVF 
         position=posRVF
+        distLeft = visual.ImageStim(win, image=imageDist[trials.thisTrial.image]+'.jpg', pos=posLVF,size=faceSize)
+        dist=distLeft
+        cue=cueRight
+        maskRight
         IM_location='RVF'
-        mask=maskRight
 
     faceStimulus= visual.ImageStim(win, image=imagefileName, pos=position,size=faceSize) #Inserts the picture into the window file, posLVF is the position, size is defined already
     
@@ -134,30 +140,34 @@ for thisTrial in trials: #Records the trials that are happening with this partic
     for frameN in range(int(round(params['duration']*params['frameRate']))): #drawing face presentation for 0.3s
         fixation.draw()
         faceStimulus.draw()
+        dist.draw()
+        cue.draw()
         win.update()
     
     for frameN in range(int(round(params['maskduration']*params['frameRate']))): #mask screen for 0.1s
         fixation.draw()
-        mask.draw()
+        maskRight.draw()
+        maskLeft.draw()
         win.update()
         
     for frameN in range(int(round(params['blankduration']*params['frameRate']))): #blank screen for 0.2 s blankdurationmL
         win.update()
         
     thisResponse = None
-#    clockRT.reset()
+    clockRT.reset()
     
     minimumdistance=10000
-    myMouse.clickReset()
 
     while thisResponse == None:                
         faceBuffer.draw()
         fixation.draw()  
-        win.update()
+        win.update() 
         buttons, RTs = myMouse.getPressed(getTime=True)
         if buttons[0]:  #left mouse click (0 is left click) (enters only if left mouse clicked)
             clickcoords=(myMouse.getPos()/2) #Again, we don't understand the reason behind coordinates doubling. Used this as a solution. This is specific for the version 2021.2.3.  
+            myMouse.clickReset()
             thisResponse=1
+            
             minimumdistance=10000 #Defining minimum distance variable for upcoming for loop 
             
             for i in range(10):
@@ -165,8 +175,8 @@ for thisTrial in trials: #Records the trials that are happening with this partic
                 
                 if distanceFaceClick<minimumdistance:
                     clickedFace = i
-                    response = clickedFace
                     minimumdistance=distanceFaceClick
+                    response=clickedFace
             if clickedFace==correctFace:
                 accuracy=1
                 corSnd.play()
